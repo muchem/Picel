2646e2c36fbcc0d1655b5d8eaab405424f49c4f0
muchemm <melissauchem@gmail.com>
2019-10-08 19:47:12 -0700

    change api property name

--- src/app/Services/images.service.ts
+++ src/app/Services/images.service.ts
@@ -8,12 +8,12 @@ import { Observable } from 'rxjs';
 })
 export class ImagesService {
   constructor(private http:HttpClient) { }
-  private apiKey: string = environment.API_KEY;
+  private ApiKey: string = environment.API_KEY;
   private Url: string = environment.URL;
   private searchTxt:string;
 
   getImages(searchTxt):Observable<any[]>{
-    return this.http.get<any[]>(`${this.Url}${this.apiKey}&q=${this.searchTxt}&image_type=photo`);
+    return this.http.get<any[]>(`${this.Url}${this.ApiKey}&q=${this.searchTxt}&image_type=photo`);
   }
 
 }
